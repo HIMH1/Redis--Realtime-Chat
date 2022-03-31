b02b828e583628505cf7593d57710db875a8f79c
Hassan Ibrahim <hassan.ibrahim.8298@gmail.com>
2022-03-31 17:10:58 +0200

    Uploading project

---
+++ realtimechat/main.py
@@ -0,0 +1,48 @@
+# HIMH 31/3/2022
+import threading
+import redis  # Redis package for Python
+import tkinter as tk
+
+redis = redis.Redis(
+    host='localhost',
+    port=6379
+)
+
+
+def submit():
+    msg = entry.get()
+    redis.set(my_username + '_last', msg)  # Add data
+    text.insert(tk.END, '\nYou: ' + msg)
+    entry.delete(0, 'end')
+
+
+my_username = 'Hassan'
+friend = 'Ali'
+window = tk.Tk()
+entry = tk.Entry()
+text = tk.Text()
+submit = tk.Button(window, text='submit', command=submit)
+last = ''
+
+
+def get_msg():
+    global last
+    value = redis.get(str(friend + '_last'))  # Get data
+    if value is not None:
+        new = value.decode('utf-8')
+        if new != last:
+            text.insert(tk.END, '\n' + friend + ': ' + new)
+            last = new
+    threading.Timer(1, get_msg).start()
+
+
+def start_chat():
+    entry.pack()
+    submit.pack()
+    text.pack()
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    get_msg()
+    start_chat()
